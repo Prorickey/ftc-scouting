74653923d99c4ffe62a566070a694c0a01f3667a
BenBean18 <beakerthespeaker@gmail.com>
2025-05-05 13:03:57 -0400

    Add function to calculate OPR for one statistic

--- stats/opr.py
+++ stats/opr.py
@@ -5,7 +5,6 @@ from typing import Callable
 from functools import reduce
 from helper import *
 
-# %%
 # Let's try calculating OPR
 # Need to set up a system of equations
 # q1_red1 + q1_red2 = red_score
@@ -60,21 +59,19 @@ def calc_opr_from_function(event_code: str, fn: Callable[[dict[database.MatchKey
         alliance_matrix[idx] = list(map(lambda team: 1 if team in match_teams[key].keys() and match_teams[key][team][1] else 0, teams_at_event))
         score_matrix[idx] = fn(match_scores[key])
 
-    # %%
     opr = np.linalg.pinv(alliance_matrix)@score_matrix
 
-    return {teams_at_event[t[0]]: t[1][0] for t in enumerate(opr)}
-# %%
-# plot it for fun!
-"""
-import matplotlib.pyplot as plt
+    return {teams_at_event[t[0]]: float(t[1][0]) for t in enumerate(opr)}
 
-plt.figure(figsize=(12,8), dpi=300)
-plt.bar(list(map(lambda t: str(t[1]), opr_associated_with_team)), list(map(lambda t: float(t[0]), opr_associated_with_team)))
-plt.xlabel("team")
-plt.ylabel("totalGamePieces")
-plt.xticks(rotation=45, fontsize=4)
-plt.savefig("totalGamePieces.png")
-plt.show()
-"""
-# %%
+def calc_single_stat_opr(event_code: str, statistic: str, season: int = 2024) -> dict:
+    """
+    Calculates OPR for every team in an event.
+
+    Args:
+        event_code: the event code of the event (used to retrieve data from database)
+        statistic: the specific score statistic to use from the FTC Event API (e.g. autoSampleNet, teleopPoints, etc)
+        season: the season the event happened
+    
+    Returns a dictionary where key = team number and value = calculated metric.
+    """
+    return calc_opr_from_function(event_code, lambda d: d[statistic], season)
