ade592849f430f2d0448fc71937af2ced58e1bac
Trevor Bedson <trevobedson@gmail.com>
2025-05-06 10:14:34 -0400

    Started working on the login page

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 
 from .teams import teams
 from .auth import auth
+from .content import content
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../static")
 
-    # Register Blueprints
+    # API Routes
     app.register_blueprint(auth, url_prefix='/api')
     app.register_blueprint(teams, url_prefix='/api/teams')
+
+    # Content Routes
+    app.register_blueprint(content, url_prefix="/")
 
     return app
